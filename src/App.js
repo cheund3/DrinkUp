f4baf3ece087a1cc83fec1119d0e263f62c4f68e
dylan <cheund3@gmail.com>
2018-10-26 15:41:50 -0400

    Enabling vibration and adding a secondary version of a scanner to test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import BarcodeScan from "./components/Scanner";
+import ScannerV2 from "./components/ScannerV2";
 
 const styles = StyleSheet.create({
   container: {
@@ -31,9 +32,10 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World</Text>
-        <Text style={styles.instructions}>Dylan Cheung</Text>
-        <BarcodeScan/>
+        {/*<Text style={styles.welcome}>Hello World</Text>*/}
+        {/*<Text style={styles.instructions}>Dylan Cheung</Text>*/}
+        {/*<BarcodeScan/>*/}
+        <ScannerV2/>
       </View>
     );
   }
