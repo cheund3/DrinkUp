56bf4034dbc000e5f8d7dbc488d6682dfdb33b36
dylan <cheund3@gmail.com>
2018-11-29 10:12:44 -0500

    Fixing scanned buttons

--- src/views/utilities/scanned/ScannedState.js
+++ src/views/utilities/scanned/ScannedState.js
@@ -2,6 +2,8 @@
 
 import {action, observable} from "mobx";
 
+const URL = "http://ec2-18-217-242-211.us-east-2.compute.amazonaws.com:3000/api/attendees/";
+
 /**
  * Scanned State
  * @author Dylan L. Cheung <cheund3@rpi.edu>
@@ -10,16 +12,51 @@ export class ScannedState {
 
   @observable title = "Scanned";
   @observable data;
-  leaving;
-
+  @observable res = "res";
 
   @action
-  enteringUser(){
-    this.leaving = false;
+  async enteringUser(data){
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: data.firstName,
+        middleName: data.middleName,
+        lastName: data.lastName,
+        dob: data.dateOfBirth,
+        licenseNumber: data.licenseNumber,
+        left: false
+      })
+    }).then(response => {
+      response.json();
+    }).catch( (error) => {
+      error;
+    });
   }
 
   @action
-  leavingUser(){
-    this.leaving = true;
+  async leavingUser(data){
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: data.firstName,
+        middleName: data.middleName,
+        lastName: data.lastName,
+        dob: data.dateOfBirth,
+        licenseNumber: data.licenseNumber,
+        left: true
+      })
+    }).then(response => {
+      response.json();
+    }).catch( (error) => {
+      error;
+    });
   }
 }
